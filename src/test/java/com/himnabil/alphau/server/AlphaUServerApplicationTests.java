754891f2dc8c62e13000d482a1bb3afb13633584
himnabil <himnabil@gmail.com>
2017-02-19 18:47:45 +0100

    finxing the build

--- src/test/java/com/himnabil/alphau/server/AlphaUServerApplicationTests.java
+++ src/test/java/com/himnabil/alphau/server/AlphaUServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.himnabil;
+package com.himnabil.alphau.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AlphauServerApplicationTests {
+public class AlphaUServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
