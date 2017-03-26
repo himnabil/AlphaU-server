8817663f990e55c6c25f73078c8dd4c973c3a78f
himnabil <himnabil@gmail.com>
2017-03-27 00:43:36 +0200

    Adding CROS GET /token

--- src/main/java/com/himnabil/alphau/server/controller/TokenController.java
+++ src/main/java/com/himnabil/alphau/server/controller/TokenController.java
@@ -7,10 +7,7 @@ import com.himnabil.alphau.server.service.PasswordUtils;
 import com.himnabil.alphau.server.service.Tokenizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -26,6 +23,7 @@ public class TokenController {
         this.passwordUtils = passwordUtils ;
     }
 
+    @CrossOrigin()
     @RequestMapping(path = "/token" , method = RequestMethod.POST)
     public ResponseEntity<String> token( @RequestBody TokenRequestBody request ) {
         User user = userRepository.find( request.getAppName() , request.getUserName());
